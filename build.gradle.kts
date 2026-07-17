5a631ee859382b4e8aa7d9560592e9bb9c7bca9f
David Lishchyshen <david@daika.dev>
2026-07-17 19:48:20 +0300

    chore: Add publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,44 @@
 plugins {
     kotlin("jvm") version "2.4.0"
+    id("com.vanniktech.maven.publish") version "0.37.0"
     kotlin("plugin.serialization") version "2.3.20"
 }
 
 group = "dev.daika"
-version = "1.0"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    pom {
+        name.set("DAVY Parsers")
+        description.set("A library for parsing player data from various sources.")
+        url.set("https://github.com/AsfhtgkDavid/davy-parsers")
+
+        licenses {
+            license {
+                name.set("Affero General Public License v3.0")
+                url.set("https://www.gnu.org/licenses/agpl-3.0.html")
+            }
+        }
+        developers {
+            developer {
+                id.set("AsfhtgkDavid")
+                name.set("David Lishchyshen")
+                email.set("david@daika.dev")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/AsfhtgkDavid/davy-parsers.git")
+            developerConnection.set("scm:git:ssh://github.com/AsfhtgkDavid/davy-parsers.git")
+            url.set("https://github.com/AsfhtgkDavid/davy-parsers")
+        }
+    }
 }
 
 dependencies {
